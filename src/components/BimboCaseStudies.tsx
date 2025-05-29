92b0244e63f94c2e1141392914193fa19b7f16d9
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-05-30 01:29:54 +0200

    Update BimboCaseStudies.tsx

--- src/components/BimboCaseStudies.tsx
+++ src/components/BimboCaseStudies.tsx
@@ -53,7 +53,7 @@ const BimboCaseStudies = () => {
             <div className="absolute inset-0 bg-[hsl(var(--image-overlay))] opacity-0 mix-blend-multiply"></div>
             
             {/* Overlay hover (solo en hover) */}
-           <div className="absolute inset-0 bg-[hsl(var(--pastel-yellow))] opacity-0 group-hover:opacity-70 transition-opacity duration-500"></div>
+           <div className="absolute inset-0 bg-[hsl(var(--pastel-yellow))] opacity-0 group-hover:opacity-80 transition-opacity duration-500"></div>
             
             {/* Texto superpuesto */}
             <div className="absolute inset-0 flex items-center justify-center p-4 sm:p-6">
