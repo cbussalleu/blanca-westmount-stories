cfe08e9d694621e529f5621fd51096a52f0f65db
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-05-29 23:29:48 +0200

    Update ProjectsData.ts

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,4 +1,3 @@
-
 export const projects = [
   {
     number: 1,
@@ -6,7 +5,7 @@ export const projects = [
     description: "Redesigned loyalty program for credit card holders to drive card usage and retention with optimized points accrual system.",
     client: "BBVA",
     slug: "loyalty-program-redesign",
-    imageSrc: "https://images.unsplash.com/photo-1589758438368-0ad531db3366?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
+    imageSrc: "/lovable-uploads/loyaltyprogram-visa.png"
   },
   {
     number: 2,
@@ -14,7 +13,7 @@ export const projects = [
     description: "Designed an operational model for producing content at global scale for Nestlé's premium coffee brands, balancing global consistency with local relevance.",
     client: "Nestlé",
     slug: "bartista",
-    imageSrc: "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
+    imageSrc: "/lovable-uploads/nespresso.png"
   },
   {
     number: 3,
@@ -22,6 +21,6 @@ export const projects = [
     description: "Redesigned end-to-end customer journey for insurance claims processing to improve satisfaction and operational efficiency.",
     client: "La Positiva",
     slug: "insurance-journey-redesign",
-    imageSrc: "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
+    imageSrc: "/lovable-uploads/lapositiva.png"
   }
 ];
