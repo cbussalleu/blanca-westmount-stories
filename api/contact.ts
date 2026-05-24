0498a033e0f6d16a00b9f1290635e86bf1fc106a
cbussalleu <christian.bussalleu@gmail.com>
2026-05-24 19:33:01 +0200

    fix: update email from address to nosilvervullets.eu domain

--- api/contact.ts
+++ api/contact.ts
@@ -16,7 +16,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   try {
     await resend.emails.send({
-      from: 'Portfolio Contact <onboarding@resend.dev>',
+      from: 'Portfolio Contact <hello@nosilvervullets.eu>',
       to: 'christian.bussalleu@gmail.com',
       replyTo: email,
       subject: subject || `New message from ${name}`,
